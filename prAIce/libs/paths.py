3fecf366272c27c5f64b35d1a52d9841aaa14342
ironcladgeek <ironcladgeek@hotmail.com>
2022-08-30 19:43:27 +0000

    Create paths module

---
+++ prAIce/libs/paths.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+SRC_DIR = ROOT_DIR / "prAIce"
+DATA_DIR = SRC_DIR / "data"
+DATA_RAW_DIR = DATA_DIR / "raw"
+LIBS_DIR = SRC_DIR / "libs"

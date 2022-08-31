cb60a0f7062b87ec66a44f24b3b590fb94678501
ironcladgeek <ironcladgeek@hotmail.com>
2022-08-31 19:34:44 +0000

    Add path for tickers folder

--- prAIce/libs/paths.py
+++ prAIce/libs/paths.py
@@ -4,4 +4,5 @@ ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 SRC_DIR = ROOT_DIR / "prAIce"
 DATA_DIR = SRC_DIR / "data"
 DATA_RAW_DIR = DATA_DIR / "raw"
+DATA_TICKERS_DIR = DATA_RAW_DIR / "tickers"
 LIBS_DIR = SRC_DIR / "libs"

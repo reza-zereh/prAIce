30845ccbdbfb6bbecf6342ba79212ca8cbd9660a
ironcladgeek <ironcladgeek@hotmail.com>
2022-09-06 10:28:05 +0000

    Add functionality to split data to Instrument class. resolve #7

--- prAIce/libs/tickers.py
+++ prAIce/libs/tickers.py
@@ -1,5 +1,7 @@
+import math
 import os
 import pathlib
+from typing import Union
 
 import pandas as pd
 import paths
@@ -414,9 +416,10 @@ class VariablesBuilder(BaseEstimator, TransformerMixin):
                     X[f"{self.source_col_}_minus_{i}_pct_change"] = X[
                         self.source_col_
                     ].pct_change(i)
-        X[f"{self.target_col_prefix}{self.forecast_period}_period"] = X[
-            self.source_col_
-        ].shift(-self.forecast_period)
+        self.target_name_ = (
+            f"{self.target_col_prefix}{self.forecast_period}_period"
+        )
+        X[self.target_name_] = X[self.source_col_].shift(-self.forecast_period)
         return X
 
 
@@ -522,12 +525,83 @@ class Instrument:
                 func(ta_obj, **kwargs)
         return ta_obj.data
 
-    def get_data(self):
+    @staticmethod
+    def __split_data(
+        data: pd.DataFrame,
+        train_size: Union[float, int] = 1.0,
+        val_size: Union[float, int] = 0.0,
+        test_size: Union[float, int] = 0.0,
+        y: Union[str, None] = None,
+        dropna: bool = False,
+    ):
+        # TODO: Write docstring
+        assert type(train_size) == type(val_size) == type(test_size), (
+            "All of the 'train_size', 'val_size', and 'test_size' "
+            "should be of same type. Either float or int."
+        )
+
+        n = len(data)
+        if type(train_size) == float:
+            assert math.isclose(
+                sum([train_size, val_size, test_size]), 1
+            ), "'train_size', 'val_size', and 'test_size' should sum up to 1.0"
+
+            val_count = int(n * val_size)
+            test_count = int(n * test_size)
+            train_count = n - (val_count + test_count)
+
+        elif type(train_size) == int:
+            assert sum([train_size, val_size, test_size]) == n, (
+                "'train_size', 'val_size', and 'test_size' should sum up "
+                f"to length of 'data' which is {n}"
+            )
+            train_count = train_size
+            val_count = val_size
+            test_count = test_size
+
+        train = data.iloc[0:train_count].copy()
+        val = data.iloc[train_count : (train_count + val_count)].copy()
+        test = data.iloc[
+            (train_count + val_count) : (train_count + val_count + test_count)
+        ].copy()
+
+        if dropna:
+            train = train.dropna()
+            val = val.dropna()
+            test = test.dropna()
+
+        y_train = y_val = y_test = None
+        if y is not None:
+            assert y in data.columns, f"'{y}' not found in axis"
+            y_train = train.pop(y)
+            y_val = val.pop(y)
+            y_test = test.pop(y)
+
+        return (
+            train,
+            val,
+            test,
+            y_train,
+            y_val,
+            y_test,
+        )
+
+    def get_data(
+        self,
+        train_size: Union[float, int] = 0.7,
+        val_size: Union[float, int] = 0.2,
+        test_size: Union[float, int] = 0.1,
+        separate_y: bool = True,
+        dropna: bool = False,
+    ):
         """Prepare the historical data with technical analysis features,
             and also lookback and forecast variables.
 
         Returns:
             pd.DataFrame: Processed DataFrame.
+
+        TODO:
+            Complete docstring
         """
         self.data_ = Ticker(ticker=self.ticker).get_history(
             period=self.period,
@@ -545,10 +619,20 @@ class Instrument:
                 X=self.data_
             )
 
-        self.data_ = VariablesBuilder(
+        vb = VariablesBuilder(
             forecast_period=self.forecast_period,
             add_past_close_prices=self.add_past_close_prices,
             add_past_pct_changes=self.add_past_pct_changes,
             lookback_period=self.lookback_period,
-        ).fit_transform(self.data_)
-        return self.data_
+        )
+        self.data_ = vb.fit_transform(self.data_)
+
+        self.splits_ = self.__split_data(
+            data=self.data_,
+            train_size=train_size,
+            val_size=val_size,
+            test_size=test_size,
+            y=vb.target_name_ if separate_y else None,
+            dropna=dropna,
+        )
+        return self.splits_

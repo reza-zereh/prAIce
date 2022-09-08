45f82b47b9c4677c2e795dc251135a8aa5ee4ac3
ironcladgeek <ironcladgeek@hotmail.com>
2022-09-08 19:53:30 +0000

    Create ml.TpotEstimator class

--- prAIce/libs/ml.py
+++ prAIce/libs/ml.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Union
 
 import flaml
 import tpot
@@ -6,7 +7,7 @@ import tpot
 
 class IEstimator(ABC):
     @abstractmethod
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, settings: Union[dict, None] = None):
         pass
 
     @abstractmethod
@@ -20,11 +21,45 @@ class FlamlEstimator(IEstimator):
     def __init__(self):
         self.estimator = flaml.AutoML()
 
-    def fit(self, X_train, y_train, settings):
+    def fit(self, X_train, y_train, settings: Union[dict, None] = None):
+        settings = {} if settings is None else settings
         self.estimator.fit(X_train=X_train, y_train=y_train, **settings)
 
     def predict(self, X_test):
         assert (
             self.estimator.model is not None
         ), "No estimator is trained. Please run fit with enough budget."
+        return self.estimator.predict(X_test)
+
+
+class TpotEstimator(IEstimator):
+    __model__ = "tpot"
+
+    def __init__(self, task: str = "regression"):
+        if task == "regression":
+            self.estimator = tpot.TPOTRegressor(
+                generations=10,
+                population_size=50,
+                scoring="neg_mean_squared_error",
+                verbosity=2,
+            )
+        elif task == "classification":
+            self.estimator = tpot.TPOTClassifier(
+                generations=10,
+                population_size=50,
+                scoring="accuracy",
+                verbosity=2,
+            )
+        else:
+            raise ValueError(
+                f"Expected 'task' to be 'regression' or 'classification', but got '{task}'."
+            )
+
+    def fit(self, X_train, y_train, settings: Union[dict, None] = None):
+        if settings is not None and type(settings) == dict:
+            self.estimator.set_params(**settings)
+
+        self.estimator.fit(X_train, y_train)
+
+    def predict(self, X_test):
         return self.estimator.predict(X_test)

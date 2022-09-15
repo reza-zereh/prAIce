3d86dd2164be893415c9fe2159dcc0c4fec49ef3
ironcladgeek <ironcladgeek@hotmail.com>
2022-09-15 21:42:56 +0000

    wip

--- notebooks/01.7__.py
+++ notebooks/01.7__.py
@@ -33,50 +33,14 @@ LIBS_DIR = ROOT_DIR / "prAIce/libs"
 sys.path.append(str(LIBS_DIR))
 # -
 
-from tickers import Instrument
-from ml import Trainer
+from train import Trainer
 
 # +
 # %%time
 
-ticker = "AAPL"
-period = "3y"
-add_ta_indicators = True
-ta_indicators_config_fn = "all_default"
-add_date_features = True
-lookback_period = 0
-add_past_close_prices = False
-add_past_pct_changes = False
-forecast_period = 1
-
-
-X_train, X_val, X_test, y_train, y_val, y_test = Instrument(
-    ticker=ticker,
-    period=period,
-    add_ta_indicators=add_ta_indicators,
-    ta_indicators_config_fn=ta_indicators_config_fn,
-    lookback_period=lookback_period,
-    forecast_period=forecast_period,
-).get_data(
-    train_size=0.95,
-    val_size=0.0,
-    test_size=0.05,
-    separate_y=True,
-    dropna=True,
+trainer = Trainer(
+    ticker="PYPL",
+    ml_models_config_fn="default_ml_config",
+    instrument_config_fn="default_instrument_config",
 )
-
-print(
-    X_train.shape,
-    X_val.shape,
-    X_test.shape,
-    y_train.shape,
-    y_val.shape,
-    y_test.shape,
-)
-# -
-
-trainer = Trainer(experiment_name="exp1", ml_models_config_fn="default")
-
-trainer.run(X_train=X_train, y_train=y_train)
-
-
+trainer.run()

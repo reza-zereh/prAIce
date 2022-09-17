280fa66a45d5b96093458e720a2cdeba01f13295
ironcladgeek <ironcladgeek@hotmail.com>
2022-09-17 12:46:39 +0000

    Update 'data/' path in paths.py

--- praice/paths.py
+++ praice/paths.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 
-ROOT_DIR = Path(__file__).resolve().parent.parent.parent
-SRC_DIR = ROOT_DIR / "prAIce"
-DATA_DIR = SRC_DIR / "data"
-DATA_RAW_DIR = DATA_DIR / "raw"
-DATA_TICKERS_DIR = DATA_RAW_DIR / "tickers"
-LIBS_DIR = SRC_DIR / "libs"
+ROOT_DIR = Path(__file__).resolve().parent.parent
+SRC_DIR = Path(__file__).resolve().parent
+
+# config files
 CONFIGS_DIR = SRC_DIR / "configs"
 TA_CONFIGS_DIR = CONFIGS_DIR / "ta_indicators"
 ML_CONFIGS_DIR = CONFIGS_DIR / "ml_models"
 INSTRUMENT_CONFIGS_DIR = CONFIGS_DIR / "instruments"
+
+# notebooks and data
+NOTEBOOKS_DIR = ROOT_DIR / "notebooks"
+DATA_DIR = NOTEBOOKS_DIR / "data"
+DATA_RAW_DIR = DATA_DIR / "raw"
+DATA_TICKERS_DIR = DATA_RAW_DIR / "tickers"

bc38d2cc8fc3627a90bd2a64137afa4db5ffbc4c
ironcladgeek <ironcladgeek@hotmail.com>
2022-09-18 11:28:06 +0000

    wip

--- praice/utils.py
+++ praice/utils.py
@@ -1,12 +1,51 @@
 import datetime
 import os
 from pathlib import PosixPath
-from typing import Union
+from typing import List, Union
 
 import yaml
 
+from . import ml
 
-def load_yaml(fp: Union[str, PosixPath]):
+
+def validate_instruments_config(config: dict):
+    pass
+
+
+def validate_learners_config(config: dict):
+    assert (
+        "learners" in config.keys()
+    ), "learners config should contain 'learners' key."
+    learners: List[dict] = config["learners"]
+    for model in learners:
+        assert (
+            "model" in model.keys()
+        ), "Each item below 'learners' should contain a 'model' key."
+        estimator_ = ml.learner(model["model"])
+        assert (
+            "settings" in model.keys()
+        ), "Each item below 'learners' should contain a 'settings' key."
+        for setting in model["settings"]:
+            assert set(setting.keys()).issubset(estimator_.valid_params), (
+                f"Provided settings ({list(setting.keys())}) is not a subset of {model['model']}"
+                f"valid parameters ({estimator_.valid_params})"
+            )
+
+
+def validate_ta_indicators_config(config: dict):
+    pass
+
+
+CONFIG_VALIDATORS = {
+    "instruments": validate_instruments_config,
+    "learners": validate_learners_config,
+    "ta_indicators": validate_ta_indicators_config,
+}
+
+
+def load_yaml(
+    fp: Union[str, PosixPath], validate: bool = False, config_type: str = None
+):
     """Load a yaml file from disk.
 
     Args:
@@ -22,6 +61,14 @@ def load_yaml(fp: Union[str, PosixPath]):
         raise FileNotFoundError(f"There is no such a file named {fp}")
     with open(str(fp), "r") as f:
         config = yaml.safe_load(f)
+
+    if validate:
+        assert (
+            config_type is not None
+            or config_type not in CONFIG_VALIDATORS.keys()
+        ), f"'config_type' should be one of {list(CONFIG_VALIDATORS.keys())} when 'validate' is True"
+        CONFIG_VALIDATORS[config_type]()
+
     return config
 
 

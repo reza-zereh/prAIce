f2df56bbb6f943a9de945328730e2ccc53a8f622
ironcladgeek <ironcladgeek@hotmail.com>
2022-09-18 09:31:24 +0000

    Write docstring for 'utils' functions.

--- praice/utils.py
+++ praice/utils.py
@@ -7,6 +7,17 @@ import yaml
 
 
 def load_yaml(fp: Union[str, PosixPath]):
+    """Load a yaml file from disk.
+
+    Args:
+        fp (Union[str, PosixPath]): File path to yaml file.
+
+    Raises:
+        FileNotFoundError: If file path is incorrect.
+
+    Returns:
+        dict: Loaded yaml.
+    """
     if not os.path.exists(fp):
         raise FileNotFoundError(f"There is no such a file named {fp}")
     with open(str(fp), "r") as f:
@@ -15,4 +26,12 @@ def load_yaml(fp: Union[str, PosixPath]):
 
 
 def format_time(seconds: Union[int, float]):
+    """Convert seconds to hh:mm:ss representation.
+
+    Args:
+        seconds (Union[int, float]): Time in seconds.
+
+    Returns:
+        str: Time in hh:mm:ss format.
+    """
     return str(datetime.timedelta(seconds=int(seconds)))

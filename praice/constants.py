d0facad50f78029a39316bd1728d1337fb86bf54
Reza Zerehpoosh <ironcladgeek@hotmail.com>
2024-08-16 10:33:27 +0000

    chore: Add `logs` directory to `PATHS` in `constants.py`

--- praice/constants.py
+++ praice/constants.py
@@ -3,4 +3,5 @@ from pathlib import Path
 PATHS = {
     "root": Path(__file__).absolute().parent.parent,
     "app": Path(__file__).absolute().parent,
+    "logs": Path(__file__).absolute().parent / "logs",
 }

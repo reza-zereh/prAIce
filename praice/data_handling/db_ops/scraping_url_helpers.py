f76d5601ce9a735723aec0cd4868826fda9f440e
Reza Zerehpoosh <ironcladgeek@hotmail.com>
2024-08-19 22:46:20 +0000

    feat: Add function to retrieve active ScrapingUrl objects by source

--- praice/data_handling/db_ops/scraping_url_helpers.py
+++ praice/data_handling/db_ops/scraping_url_helpers.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from praice.data_handling.db_ops.crud import get_symbol
 from praice.data_handling.models import ScrapingUrl
 
@@ -19,4 +21,21 @@ def get_scraping_url_by_symbol_and_source(symbol: str, source: str) -> ScrapingU
     symbol_obj = get_symbol(symbol)
     return ScrapingUrl.get(
         (ScrapingUrl.symbol == symbol_obj) & (ScrapingUrl.source == source)
+    )
+
+
+def get_active_scraping_urls_by_source(source: str) -> List[ScrapingUrl]:
+    """
+    Retrieves all active ScrapingUrl objects for the given source.
+
+    Args:
+        source (str): The source to search for.
+
+    Returns:
+        list: A list of active ScrapingUrl objects for the given source.
+    """
+    return list(
+        ScrapingUrl.select().where(
+            (ScrapingUrl.source == source) & (ScrapingUrl.is_active == True)  # noqa: E712
+        )
     )

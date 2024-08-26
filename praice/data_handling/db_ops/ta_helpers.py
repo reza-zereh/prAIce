e5d5339842b9c54037bf314de7236e6ffe1b30fd
Reza Zerehpoosh <ironcladgeek@hotmail.com>
2024-08-26 09:37:10 +0000

    feat: Add `calculate_and_store_technical_analysis_for_all_symbols` function to `ta_helpers.py`

--- praice/data_handling/db_ops/ta_helpers.py
+++ praice/data_handling/db_ops/ta_helpers.py
@@ -1,6 +1,8 @@
 import datetime
 from typing import Union
 
+from loguru import logger
+
 from praice.data_handling.db_ops import crud, historical_price_helpers
 from praice.data_handling.db_ops.crud import Timeframe
 from praice.data_handling.models import Symbol, TechnicalAnalysis
@@ -25,6 +27,8 @@ def calculate_and_store_technical_analysis(
     Returns:
         int: The number of records upserted.
     """
+    logger.info(f"Calculating and storing technical analysis for {symbol}")
+
     # get all historical prices
     df = historical_price_helpers.get_historical_prices_df(symbol)
     # calculate TA for the whole range but only get the processed data within the date range
@@ -34,6 +38,36 @@ def calculate_and_store_technical_analysis(
     upsert_count = crud.bulk_upsert_technical_analysis(
         symbol=symbol, data=processed_data, timeframe=timeframe
     )
+    return upsert_count
+
+
+def calculate_and_store_technical_analysis_for_all_symbols(
+    start_date: Union[datetime.date, str] = None,
+    end_date: Union[datetime.date, str] = None,
+    timeframe: Timeframe = Timeframe.DAYS_1.value,
+) -> int:
+    """
+    Calculate and store technical analysis data for all symbols within a date range
+        that have collect_technical_indicators set to True in the SymbolConfig table.
+
+    Args:
+        start_date (Union[datetime.date, str], optional): The start date of the range (inclusive).
+        end_date (Union[datetime.date, str], optional): The end date of the range (inclusive).
+        timeframe (Timeframe, optional): The timeframe for the technical analysis data.
+
+    Returns:
+        int: The number of records upserted.
+    """
+    configs = crud.list_symbol_configs()
+    upsert_count = 0
+    for config in configs:
+        if config.collect_technical_indicators:
+            upsert_count += calculate_and_store_technical_analysis(
+                symbol=config.symbol.symbol,
+                start_date=start_date,
+                end_date=end_date,
+                timeframe=timeframe,
+            )
     return upsert_count
 
 

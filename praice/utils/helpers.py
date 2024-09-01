f502f68c5799ef30fae71c6a59c65dc780a3ff0b
Reza Zerehpoosh <ironcladgeek@hotmail.com>
2024-09-01 19:50:19 +0000

    refactor: Add validation for `chunk_size` parameter in `chunked()` function

--- praice/utils/helpers.py
+++ praice/utils/helpers.py
@@ -1,4 +1,7 @@
 def chunked(iterable, chunk_size):
     """Yield successive chunk_size-sized chunks from iterable."""
+    if chunk_size <= 0:
+        raise ValueError("Chunk size must be greater than 0")
+
     for i in range(0, len(iterable), chunk_size):
         yield iterable[i : i + chunk_size]

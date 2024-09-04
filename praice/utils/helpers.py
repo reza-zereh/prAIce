a48de94dddfae5fd7eadf47c46c1dd74e696565f
Reza Zerehpoosh <ironcladgeek@hotmail.com>
2024-09-04 13:54:57 +0000

    refactor: Add `count_words` function to helpers.py module

--- praice/utils/helpers.py
+++ praice/utils/helpers.py
@@ -1,3 +1,6 @@
+import re
+
+
 def chunked(iterable, chunk_size):
     """Yield successive chunk_size-sized chunks from iterable."""
     if chunk_size <= 0:
@@ -5,3 +8,8 @@ def chunked(iterable, chunk_size):
 
     for i in range(0, len(iterable), chunk_size):
         yield iterable[i : i + chunk_size]
+
+
+def count_words(text: str) -> int:
+    """Count the number of words in a text string."""
+    return len(re.findall(r"\w+", text))

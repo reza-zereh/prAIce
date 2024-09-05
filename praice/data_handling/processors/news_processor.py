aa5279588628b2a910a45e7f8048825275108254
Reza Zerehpoosh <ironcladgeek@hotmail.com>
2024-09-05 22:02:09 +0000

    feat: Populate content summary for news entries

--- praice/data_handling/processors/news_processor.py
+++ praice/data_handling/processors/news_processor.py
@@ -1,4 +1,5 @@
 from praice.data_handling.models import News, db
+from praice.libs.summarizers import SummarizerFactory
 from praice.utils import helpers
 
 
@@ -16,10 +17,6 @@ def populate_words_count(batch_size: int = 200) -> int:
 
     Returns:
         int: The total number of entries updated.
-
-    Note:
-        This function uses a database transaction for efficiency and
-        to ensure data consistency.
     """
     total_updated = 0
 
@@ -36,5 +33,34 @@ def populate_words_count(batch_size: int = 200) -> int:
                 news.words_count = word_count
                 news.save()
                 total_updated += 1
+
+    return total_updated
+
+
+def populate_content_summary(
+    news_min_words_count=300, summary_max_tokens=200, limit=5
+) -> int:
+    """
+    Populates the content summary of news entries.
+
+    Args:
+        news_min_words_count (int): The minimum number of words required for a news entry to be considered.
+        summary_max_tokens (int): The maximum number of tokens to include in the summary.
+        limit (int): The maximum number of news entries to process.
+
+    Returns:
+        int: The total number of news entries whose content summary was updated.
+    """
+    summarizer = SummarizerFactory.get_summarizer("bart")
+    total_updated = 0
+
+    # Query to get N news entries with words_count greater than or equal to news_min_words_count
+    query = News.select().where(News.words_count >= news_min_words_count).limit(limit)
+
+    for news in query:
+        summary = summarizer.summarize(text=news.content, max_tokens=summary_max_tokens)
+        news.content_summary = summary
+        news.save()
+        total_updated += 1
 
     return total_updated

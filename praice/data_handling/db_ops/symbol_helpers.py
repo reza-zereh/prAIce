f62124f9b9cd47cebab94af5523babf856fe1bb5
Reza Zerehpoosh <ironcladgeek@hotmail.com>
2024-09-13 10:36:33 +0000

    refactor: Provide `description` when adding a symbol in `create_symbol_from_yahoo()` function

--- praice/data_handling/db_ops/symbol_helpers.py
+++ praice/data_handling/db_ops/symbol_helpers.py
@@ -56,6 +56,7 @@ def create_symbol_from_yahoo(symbol: str) -> Symbol:
         sector=info.get("sector"),
         industry=info.get("industry"),
         exchange=info.get("exchange"),
+        description=info.get("longBusinessSummary"),
     )
 
     logger.info(f"Created new symbol: {new_symbol.symbol}")

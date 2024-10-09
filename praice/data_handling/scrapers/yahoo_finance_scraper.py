7808672660a5f8ad7534254b2836c479aedc8884
Reza Zerehpoosh <ironcladgeek@hotmail.com>
2024-10-09 19:25:51 +0000

    refactor: Update YahooFinanceScraper to scrape news items with new class name

--- praice/data_handling/scrapers/yahoo_finance_scraper.py
+++ praice/data_handling/scrapers/yahoo_finance_scraper.py
@@ -28,7 +28,9 @@ class YahooFinanceScraper(NewsScraper):
         soup = self.get_soup(url)
         news_list = []
 
-        for news_item in soup.find_all("li", class_="stream-item yf-ovk92u"):
+        for news_item in soup.find_all(
+            "li", class_="stream-item story-item yf-1usaaz9"
+        ):
             headline_tag = news_item.find("h3", class_="clamp yf-1sxfjua")
             if headline_tag:
                 headline = headline_tag.text.strip()
